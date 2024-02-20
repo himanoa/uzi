b5cd71281af899a88fc40dd7dd828bf414cfd8be
himanoa <matsunoappy@gmail.com>
2024-02-20 21:46:01 +0900

    Effecful, saiko～～～～～

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,12 @@
 module Lib
-    ( someFunc
+    ( runUzi
     ) where
+import Effectful (runEff)
+import qualified Control.Applicative as Eff
+import qualified Control.Monad.IO.Class as Eff
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+runUzi :: IO ()
+
+runUzi = runEff $ do
+  _ <- Eff.liftIO $ putStrLn "someFunc"
+  Eff.pure()
