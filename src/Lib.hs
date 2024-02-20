91b99008c64a9cbea1c2a587352bb2e85327a726
himanoa <matsunoappy@gmail.com>
2024-02-21 01:54:50 +0900

    Define OpcodeSpec

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,6 +3,7 @@
 
 module Lib
     ( runUzi,
+      module Opcode
     ) where
 
 import Opcode
@@ -15,49 +16,46 @@ import Data.Text.Encoding (decodeUtf8)
 import Control.Exception (throw, Exception)
 import Network.WebSockets (Connection)
 
-runUzi :: IO ()
-runUzi = putStrLn "Unchi"
 
--- 
--- data EnvConfig = EnvConfig {
---   discordApiToken :: Text
--- }
---   deriving Show
--- 
--- makeEnvConfig :: Text -> EnvConfig 
--- makeEnvConfig discordApiToken = EnvConfig { discordApiToken  }
--- 
--- fromEnvironment :: Environment :> es => Eff es EnvConfig
--- 
--- data FromEnvironmentError = DiscordApiTokenIsUndefined
---   deriving Show
--- instance Exception FromEnvironmentError
--- 
--- convertToText :: String -> Text
--- convertToText = decodeUtf8 . ByteString.pack
--- 
--- fromEnvironment = do
---   discordApiTokenMaybe <- lookupEnv "UZI_DISCORD_API_TOKEN"
---   discordApiToken <- maybe (throw DiscordApiTokenIsUndefined) pure discordApiTokenMaybe
---   pure $ makeEnvConfig . convertToText $ discordApiToken
--- 
--- 
--- runClient:: MonadUnliftIO m => String -> Int -> String -> WS.ConnectionOptions ->  WS.Headers -> (Connection -> m a) -> m a
--- runClient host port path opt headers inner = 
---   withRunInIO $ \runInIO ->
---   WS.runClientWith host port path opt headers (runInIO . inner) 
--- 
--- runUzi :: IO ()
--- runUzi = runEff $ do
---   -- withStdOutLogger $ \stdoutLogger -> do
---     runEnvironment $ do 
---       -- runLog "Uzi" stdoutLogger defaultLogLevel $ do 
--- --        logInfo_ "Hello uzi"
--- --        logInfo_ "Load enviroments"
---       config <- fromEnvironment
---  --       _ <- logInfo_ . pack . show $ config
---       runClient "localhost" 1337 ""  WS.defaultConnectionOptions [("xxx", "yyy")] $ \c -> do
---         _ <- liftIO (WS.sendTextData c (ByteString.pack "xxxx"))
---         pure()
--- --          logInfo_ "Connected websocket"
---       
+data EnvConfig = EnvConfig {
+  discordApiToken :: Text
+}
+  deriving Show
+
+makeEnvConfig :: Text -> EnvConfig 
+makeEnvConfig discordApiToken = EnvConfig { discordApiToken  }
+
+fromEnvironment :: Environment :> es => Eff es EnvConfig
+
+data FromEnvironmentError = DiscordApiTokenIsUndefined
+  deriving Show
+instance Exception FromEnvironmentError
+
+convertToText :: String -> Text
+convertToText = decodeUtf8 . ByteString.pack
+
+fromEnvironment = do
+  discordApiTokenMaybe <- lookupEnv "UZI_DISCORD_API_TOKEN"
+  discordApiToken <- maybe (throw DiscordApiTokenIsUndefined) pure discordApiTokenMaybe
+  pure $ makeEnvConfig . convertToText $ discordApiToken
+
+
+runClient:: MonadUnliftIO m => String -> Int -> String -> WS.ConnectionOptions ->  WS.Headers -> (Connection -> m a) -> m a
+runClient host port path opt headers inner = 
+  withRunInIO $ \runInIO ->
+  WS.runClientWith host port path opt headers (runInIO . inner) 
+
+runUzi :: IO ()
+runUzi = runEff $ do
+  -- withStdOutLogger $ \stdoutLogger -> do
+    runEnvironment $ do 
+      -- runLog "Uzi" stdoutLogger defaultLogLevel $ do 
+--        logInfo_ "Hello uzi"
+--        logInfo_ "Load enviroments"
+      config <- fromEnvironment
+ --       _ <- logInfo_ . pack . show $ config
+      runClient "localhost" 1337 ""  WS.defaultConnectionOptions [("xxx", "yyy")] $ \c -> do
+        _ <- liftIO (WS.sendTextData c (ByteString.pack "xxxx"))
+        pure()
+--          logInfo_ "Connected websocket"
+      
---
+++ test/OpcodeSpec.hs
@@ -0,0 +1,57 @@
+module OpcodeSpec (
+  spec
+) where
+
+import Test.Hspec 
+import Lib (OperationCode(Identify, SelectProtocol, Ready, Heartbeat, SessionDescription, Speaking, HeartbeatAck, Resume, Hello, Resumed, ClientConnect, ClientDisconnect))
+
+spec :: Spec
+spec = describe "Opcode" $ do
+  describe "fromEnum" $ do
+    describe "input is Identify" $ do
+      it "is return to 0" $ do
+        fromEnum Identify `shouldBe` 0
+    
+    describe "input is SelectProtocol" $ do
+      it "is return to 1" $ do
+        fromEnum SelectProtocol `shouldBe` 1
+
+    describe "input is Ready" $ do
+      it "is return to 2" $ do
+        fromEnum Ready `shouldBe` 2
+
+    describe "input is Heartbeat" $ do
+      it "is return to 3" $ do
+        fromEnum Heartbeat `shouldBe` 3
+
+    describe "input is SessionDescription" $ do
+      it "is return to 4" $ do
+        fromEnum SessionDescription `shouldBe` 4
+
+    describe "input is Speaking" $ do
+      it "is return to 5" $ do
+        fromEnum Speaking `shouldBe` 5
+
+    describe "input is HeartbeatAck" $ do
+      it "is return to 6" $ do
+        fromEnum HeartbeatAck `shouldBe` 6
+
+    describe "input is Resume" $ do
+      it "is return to 7" $ do
+        fromEnum Resume `shouldBe` 7
+
+    describe "input is Hello" $ do
+      it "is return to 8" $ do
+        fromEnum Hello `shouldBe` 8
+
+    describe "input is Resumed" $ do
+      it "is return to 9" $ do
+        fromEnum Resumed `shouldBe` 9
+
+    describe "input is ClientConnect" $ do
+      it "is return to 10" $ do
+        fromEnum ClientConnect `shouldBe` 10
+
+    describe "input is ClientDisconnect" $ do
+      it "is return to 11" $ do
+        fromEnum ClientDisconnect `shouldBe` 11
