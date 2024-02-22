a68ac6fe9fde0a579b02dbc435aba5150945c8e1
himanoa <matsunoappy@gmail.com>
2024-02-23 00:01:48 +0900

    Add failed test case

--- test/Data/Discord/Response/HelloEventResponseSpec.hs
+++ test/Data/Discord/Response/HelloEventResponseSpec.hs
@@ -10,7 +10,12 @@ spec::Spec
 spec = describe "HelloEventResponse" $ do 
   describe "FromJson" $ do
     describe "parseJSON" $ do
-      it "is return to Just HelloEventResponse" $ do
-        let json = "{\"op\": 10}"
-        Data.Aeson.decode @HelloEventResponse json `shouldBe` Just HelloEventResponse
+      describe "if op equal 10" $ do
+        it "is return to Just HelloEventResponse" $ do
+          let json = "{\"op\": 10}"
+          Data.Aeson.decode @HelloEventResponse json `shouldBe` Just HelloEventResponse
+      describe "if op equal 11" $ do
+        it "is return to Just HelloEventResponse" $ do
+          let json = "{\"op\": 11}"
+          Data.Aeson.decode @HelloEventResponse json `shouldBe` Nothing
 
