228103658278228a374be55ef8015fab344124b6
himanoa <matsunoappy@gmail.com>
2024-02-24 18:12:05 +0900

    Use Effectful.DiscordGateway instead of UnliftWuss module

--- src/Lib.hs
+++ src/Lib.hs
@@ -14,21 +14,17 @@ import Control.Monad (forever)
 import Data.ByteString.Char8 qualified as ByteString
 import Data.Text
 import Data.Text.Encoding (decodeUtf8)
-import Effectful (Eff, IOE, liftIO, runEff, (:>))
+import Effectful (Eff, IOE, runEff, (:>))
 import Effectful.Concurrent.Async ( concurrently_, runConcurrent )
 import Effectful.Environment (Environment, lookupEnv, runEnvironment)
 import Effectful.Log
 import Log.Backend.StandardOutput
 import Network.WebSockets (Connection)
-import Network.WebSockets qualified as WS
 import Opcode
-import UnliftWuss qualified
-import Data.Discord.Response.HelloEventResponse
-import Data.Aeson (eitherDecode)
-import qualified Data.Text.Encoding as LT
-import Data.ByteString.Lazy qualified as BL
 import Effectful.Concurrent (Concurrent)
 import Effectful.Concurrent.STM (TQueue, atomically, writeTQueue, newTQueue, readTQueue)
+import Effectful.DiscordGateway
+import Data.Discord
 
 data EnvConfig = EnvConfig
   {discordApiToken :: Text}
@@ -71,47 +67,34 @@ startUp = do
   _ <- logInfo_ "Load enviroments"
   config <- fromEnvironment
   _ <- logInfo_ . pack . show $ config
-
-  _ <-
-    UnliftWuss.runClient
-      "gateway.discord.gg"
-      443
-      "/?v=10&encoding-json"
-      WS.defaultConnectionOptions
-      []
-      onConnect
-      `catch` ( \(e :: WS.HandshakeException) -> do
-                  _ <- logAttention_ $ pack ("handshake failed " <> displayException e)
-                  throw CrashError
-              )
+  withDiscordGatewayConnection onConnect
   pure ()
 
 onConnect :: (Log :> es, IOE :> es, Concurrent :> es) => Connection -> Eff es ()
 onConnect c = do
   _ <- logInfo_ "Connected websocket"
-  _ <- UnliftWuss.withPingThread c 15 (pure ()) $ do
+  _ <- withPingThread c 15 (pure ()) $ do
     queue <- atomically newTQueue
-    concurrently_ (forever $ receiver c queue) (forever $ sender c queue)
+    concurrently_ (forever (runDiscordGateway c (receiver queue))) (forever $ runDiscordGateway c (sender queue))
   -- TODO: Handle Ctrl + C and kill signal
   pure ()
 
-receiver :: (Log :> es, IOE :> es, Concurrent :> es) => Connection -> TQueue HelloEventResponse -> Eff es ()
-receiver conn queue = do
-  d <- liftIO (WS.receiveData conn)
-  _ <- case eitherDecode @HelloEventResponse (BL.fromStrict . LT.encodeUtf8 $ d) of
-    Right x ->  atomically $ writeTQueue queue x
-    Left _-> pure ()
-  _ <- logInfo_ d
+receiver :: (Log :> es, IOE :> es, Concurrent :> es, DiscordGateway :> es) => TQueue Response -> Eff es ()
+receiver queue = do
+  event <- receiveEvent
+  _ <- case event of
+    Just x ->  atomically $ writeTQueue queue x
+    Nothing -> pure ()
   pure ()
 
 
 data SenderError = EventQueueIsEmpty
   deriving (Show)
 
-sender :: (Log :> es, IOE :> es, Concurrent :> es) => Connection -> TQueue HelloEventResponse ->  Eff es ()
-sender conn queue = do
+sender :: (Log :> es, IOE :> es, Concurrent :> es, DiscordGateway :> es) => TQueue Response ->  Eff es ()
+sender queue = do
   event <- atomically $ readTQueue queue
-  logInfo_ . convertToText $ ("Received Log" <> show event)
+  logInfo_ . convertToText $ ("Received Log " <> show event)
   -- case eventMaybe of
   --   Just event ->  logInfo_ . convertToText $ ("Received Log" <> show event)
   --   Nothing -> pure ()
