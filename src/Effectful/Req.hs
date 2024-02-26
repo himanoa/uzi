5661c3220af43b4f066a66e994f1e20d3f1a05e8
himanoa <matsunoappy@gmail.com>
2024-02-26 13:15:32 +0900

    Implemented Req effect

--- src/Effectful/Req.hs
+++ src/Effectful/Req.hs
@@ -1,26 +1,48 @@
+{-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
+
 module Effectful.Req (
   request,
-  Request
+  Request,
+  runRequest
 ) where
 import Effectful
-import Effectful.Dispatch.Static
-import Network.HTTP.Req
+import Effectful.Dispatch.Dynamic
+import Network.HTTP.Req qualified as R
 import Data.Proxy
+import Effectful.Internal.Monad (unsafeEff_)
 
-data Request :: Effect
 
-type instance DispatchOf Request = Static WithSideEffects
+data Request :: Effect where
+  Req ::
+    ( R.HttpMethod method,
+      R.HttpBody body,
+      R.HttpResponse response,
+      R.HttpBodyAllowed (R.AllowsBody method) (R.ProvidesBody body)
+    ) =>
+    method ->
+    R.Url scheme ->
+    body ->
+    Proxy response ->
+    R.Option scheme ->
+    Request m response
+
+type instance DispatchOf Request = Dynamic
 
 request :: (
-  IOE :> es,
-  HttpMethod method,
-  HttpBody body,
-  HttpResponse response,
-  HttpBodyAllowed (AllowsBody method) (ProvidesBody body)
-  ) =>method -> Url scheme -> body -> Proxy response -> Eff es response
-request method url body response = do
-  r <- runReq defaultHttpConfig $ 
-    req method url body response mempty
-  unsafeEff_ $ pure r
+  Request :> es,
+  R.HttpMethod method,
+  R.HttpBody body,
+  R.HttpResponse response,
+  R.HttpBodyAllowed (R.AllowsBody method) (R.ProvidesBody body)
+  ) =>method -> R.Url scheme -> body -> Proxy response -> R.Option scheme ->  Eff es response
+request method url body response scheme =  send ( Req method url body response scheme )
+
+runRequest :: Eff (Request : es) a -> Eff es a
+runRequest = interpret $ \_ -> \case
+  Req method url body r o ->  unsafeEff_ . R.runReq R.defaultHttpConfig $ do
+    R.req method url body r o
--- src/EventHandler.hs
+++ src/EventHandler.hs
@@ -11,12 +11,11 @@ import Effectful.DiscordGateway
 import Effectful
 import EnvConfig
 import Data.Discord.Response
-import EventHandler.MessageCreateEventHandler (dispatchMessageEventHandlers)
 import Effectful.NonDet 
 
 dispatchEventHandlers :: (DiscordGateway :> es, DynamicLogger :> es) => EnvConfig -> Response -> Eff es ()
 dispatchEventHandlers c r = do
-  runNonDet OnEmptyKeep (readyEventHandler c r <|> helloEventHandler c r)
+  _ <- runNonDet OnEmptyKeep (readyEventHandler c r <|> helloEventHandler c r)
   pure ()
 --  dispatchMessageEventHandlers c r
 
