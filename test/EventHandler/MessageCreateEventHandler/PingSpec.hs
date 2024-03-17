1dd59d0a47ee5215d59ab3d9f6f5422ce97d0115
himanoa <matsunoappy@gmail.com>
2024-03-17 20:37:54 +0900

    Fix broken test

--- test/EventHandler/MessageCreateEventHandler/PingSpec.hs
+++ test/EventHandler/MessageCreateEventHandler/PingSpec.hs
@@ -16,6 +16,7 @@ import Effectful
 import Effectful.DiscordChannel.Effect hiding (roles)
 import Effectful.NonDet
 import Effectful.State.Static.Local
+import Effectful.DynamicLogger
 import EventHandler.MessageCreateEventHandler.Ping (pingEventHandler)
 import Helper.DummyDiscordChannelInterpreter
 import Test.Hspec
@@ -26,13 +27,13 @@ spec = describe "PingSpec" $ do
     context "when provide not MessageCreateEvent" $ do
       it "should be return to emptyEff" $ do
         let response = Hello HelloEventResponse
-        let actual = runPureEff . runNonDet OnEmptyKeep . runState @(Maybe SendMessageParams) Nothing . runDummyDiscordChannel $ pingEventHandler response
+        let actual = runPureEff . runSilentDynamicLogger . runNonDet OnEmptyKeep . runState @(Maybe SendMessageParams) Nothing . runDummyDiscordChannel $ pingEventHandler response
         isLeft actual `shouldBe` True
     context "when provide MessageCreateEvent" $ do
       context "when response message is ping" $ do
         it "should be return to pong response" $ do
           let response = makeMessageCreateEventResponse (ChannelId "xxx") (makeUnsafeContent "ping") [] Member {roles = [], nick = Just . Nickname $ "himanoa"} True
-          let actual = runPureEff . runNonDet OnEmptyKeep . runState @(Maybe SendMessageParams) Nothing . runDummyDiscordChannel . pingEventHandler . MessageCreate $ response
+          let actual = runPureEff . runSilentDynamicLogger . runNonDet OnEmptyKeep . runState @(Maybe SendMessageParams) Nothing . runDummyDiscordChannel . pingEventHandler . MessageCreate $ response
           let (_, paramsMaybe) = fromRight ((), Nothing) actual
           let params = fromJust paramsMaybe
 
@@ -41,6 +42,6 @@ spec = describe "PingSpec" $ do
       context "when response message is not ping" $ do
         it "should be return to pong response" $ do
           let response = makeMessageCreateEventResponse (ChannelId "xxx") (makeUnsafeContent "dummy") [] Member {roles = [], nick = Just . Nickname $ "himanoa"} True
-          let actual = runPureEff . runNonDet OnEmptyKeep . runState @(Maybe SendMessageParams) Nothing . runDummyDiscordChannel . pingEventHandler . MessageCreate $ response
+          let actual = runPureEff . runSilentDynamicLogger. runNonDet OnEmptyKeep . runState @(Maybe SendMessageParams) Nothing . runDummyDiscordChannel . pingEventHandler . MessageCreate $ response
 
           isLeft actual `shouldBe` True
