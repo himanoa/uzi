01365e230ad0d550e633c5c98c99007e18c62be9
himanoa <matsunoappy@gmail.com>
2024-03-21 21:08:49 +0900

    wip

--- src/EventHandler/HelloEventHandler.hs
+++ src/EventHandler/HelloEventHandler.hs
@@ -14,14 +14,22 @@ import Effectful.DiscordApiTokenReader (DiscordApiTokenReader, getToken)
 import Effectful.DiscordGateway (DiscordGateway, sendEvent)
 import Effectful.DynamicLogger
 import Effectful.NonDet
+import Effectful.Concurrent
+import Data.Aeson (object)
+import Data.Aeson.Types
 
 convertToText :: String -> Text
 convertToText = decodeUtf8 . ByteString.pack
 
-helloEventHandler :: (DiscordGateway :> es, DynamicLogger :> es, NonDet :> es, DiscordApiTokenReader :> es) => Response -> Eff es ()
+helloEventHandler :: (DiscordGateway :> es, DynamicLogger :> es, NonDet :> es, DiscordApiTokenReader :> es, Concurrent :> es) => Response -> Eff es ()
 helloEventHandler = \case
   Hello _ -> do
     discordApiToken <- getToken
     sendEvent . Identify . defaultIdentifyRequest $ discordApiToken
     info "Sent HelloEvetnt"
   _ -> emptyEff
+
+sendHeartbeat  :: (DiscordGateway :> es) => Int -> Eff es ()
+sendHeartbeat interval = do
+  let heartbeartObject = object ["op" .= (1 :: Int), "d" .= (251 :: Int)]
+  undefined
