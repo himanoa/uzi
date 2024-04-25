59cce253229fd103efb8ef96c13028e2236d0321
himanoa <matsunoappy@gmail.com>
2024-04-26 03:46:33 +0900

    BotUserEffectのドキュメントを追加

--- src/Effectful/BotUser/Effect.hs
+++ src/Effectful/BotUser/Effect.hs
@@ -2,6 +2,16 @@
 {-# LANGUAGE TypeFamilies #-}
 {-# OPTIONS_GHC -Wno-missing-export-lists #-}
 
+{-|
+ Module: Effectful.BotUser.Effect
+ Description: UziBotがログインしているDiscordアカウントの情報へアクセスするEffect
+ Maintainer: himanoa <matsunoappy@gmail.com>
+
+ UziBotがログインしているDiscordアカウントの情報へアクセスするEffectです。
+
+ Botがログインしているユーザーが確定するのはUziを起動してDiscordからReadyEventを受け取った後です。
+-}
+
 module Effectful.BotUser.Effect where
 
 import Data.Discord.User
@@ -15,8 +25,14 @@ data BotUser :: Effect where
 
 type instance DispatchOf BotUser = Dynamic
 
+-- | Botがログイン中のDiscordアカウントを更新します
+--
+-- 'EventHandler.ReadyEventHandler.readyEventHandler'以外からは呼び出さないでください
 setBotUser :: (HasCallStack, BotUser :> es) => User -> Eff es ()
 setBotUser = send . SetBotUser
 
+-- | Botがログイン中のDiscordアカウントの情報を取得します
+--
+-- ログイン中のDiscordアカウントが確定しない場合は'Nothing'を返します
 getBotUser :: (HasCallStack, BotUser :> es) => Eff es (Maybe User)
 getBotUser = send GetBotUser
